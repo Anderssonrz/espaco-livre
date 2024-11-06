9d071092fce433481862eb9e5dc2def943ffec67
Anderson <141687227+Anderssonrz@users.noreply.github.com>
2024-11-06 11:43:52 -0300

    add

--- busca.php
+++ busca.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Encontre Seu Estacionamento - Airbnb de Estacionamento</title>
+    <title>Encontre Seu Estacionamento</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="styles.css">
 </head>
