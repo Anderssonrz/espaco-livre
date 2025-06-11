4127ded6c1ddbcca5461bd8651b7c47b3ac61a7e
Carlos-Godoi <168770521+Carlos-Godoi@users.noreply.github.com>
2025-06-11 17:57:18 -0300

    Links Bootstraps
    Página login

--- login.php
+++ login.php
@@ -32,6 +32,10 @@ if (isset($_POST['btnCadastrar'])) {
   <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="./assets/css/login.css" />
   <title>Espaço Livre</title>
+  <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/styles.css" rel="stylesheet" />
 </head>
 
 <body>
